383d9aaa35478318dc075146e5ddf3d854959521
haydr <haydr@sas.upenn.edu>
2026-04-09 15:38:04 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -69,8 +69,8 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Make a simple circle
-var circle = L.circle([-71.12266472959821,
-          42.373113451801515], 
+var circle = L.circle([42.373113451801515, 
+                       -71.12266472959821], 
 { color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
